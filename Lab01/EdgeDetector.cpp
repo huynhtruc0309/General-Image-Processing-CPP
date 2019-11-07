a4af5f457aeec89a8049368bc42aaab1dd83e12d
ontheskyl <38610247+ontheskyl@users.noreply.github.com>
2019-11-07 08:50:33 +0700

    Update EdgeDetector.cpp

--- Lab01/EdgeDetector.cpp
+++ Lab01/EdgeDetector.cpp
@@ -6,7 +6,36 @@ int EdgeDetector::DetectEdge(const Mat & sourceImage, Mat & destinationImage, in
 {
 	if (sourceImage.empty()) //Kiểm tra hình đâu vào và thoát ra nếu lỗi
 		return 0;
+	if (method == 1) // Sobel Edge Detection
+	{
+		destinationImage.create(sourceImage.size(), sourceImage.type());
+		vector<vector<int>> matrix;
+		matrix.resize(sourceImage.rows);
+		for (int i = 0; i < sourceImage.rows; i++)
+		{// Lưu ma trận của ảnh sourceImage
+			matrix[i].resize(sourceImage.cols);
+			for (int k = 0; k < sourceImage.cols; k++)
+			{
+				matrix[i][k] = static_cast<int>(sourceImage.at<uchar>(i, k));
+			}
+		}
 
+		for (int i = 1; i < sourceImage.rows - 1; i++)
+		{// Tính toán theo mặt nạ Sobel để có được biên cạnh
+			for (int k = 1; k < sourceImage.cols - 1; k++)
+			{
+				int Gx = matrix[i - 1][k + 1] + 2 * matrix[i][k + 1] + matrix[i + 1][k + 1];
+				Gx -=    matrix[i - 1][k - 1] + 2 * matrix[i][k - 1] + matrix[i + 1][k - 1];
+				int Gy = matrix[i + 1][k - 1] + 2 * matrix[i + 1][k] + matrix[i + 1][k + 1];
+				Gy -=    matrix[i - 1][k - 1] + 2 * matrix[i - 1][k] + matrix[i - 1][k + 1];
+				
+				float alpha = round(sqrt(Gx*Gx + Gy*Gy));// Có thể chọn alpha = |Gx| + |Gy|
+				destinationImage.at<uchar>(i, k) = static_cast<uchar>(alpha);
+			}
+		}
+	}
+	
+	
 	Convolution conv;
 	Mat fx, fy;
 	vector<float> Wx = { -1/3, 0, 1/3,
